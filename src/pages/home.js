07f01a7209e61c36abfed18a911421bfdd4fefd5
David <david@Davids-Air-2.fios-router.home>
2023-08-30 16:17:12 -0400

    home edits

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,8 @@
-import {Container, Box, Heading, Text, SimpleGrid, Image, Center, HStack, VStack } from "@chakra-ui/react"
+import {Container, Box, Heading, Text, SimpleGrid, Image, Center, HStack, VStack, List, ListItem, ListIcon, Link as ChakraLink} from "@chakra-ui/react"
 import React from 'react'
+import { IconBrandLinkedin, IconBrandTwitter, IconMessage} from "@tabler/icons-react";
 import dbImage from '../images/headshot.jpg'
+import { Link } from "react-router-dom";
 export default function Home() {
     return(
         
@@ -23,8 +25,20 @@ export default function Home() {
                     <Text textAlign='justify' style={{textIndent: 20}}>{'\tDo commodo eiusmod amet amet proident qui consequat veniam occaecat excepteur enim dolor eu. Dolore eu aliquip occaecat ut sit fugiat. Reprehenderit anim consectetur excepteur duis veniam. Ipsum proident ullamco enim aute in amet pariatur officia consectetur. Aliqua nulla elit deserunt esse. Eiusmod culpa sunt cillum est velit ex magna veniam.'}</Text>
                 </Box>
                 <Box>
-                    <Heading as='h4' fontSize='2xl' textAlign='left'>Contact</Heading>
-                    <Text textAlign='justify' style={{textIndent: 20}}>{'\tDo commodo eiusmod amet amet proident qui consequat veniam occaecat excepteur enim dolor eu. Dolore eu aliquip occaecat ut sit fugiat. Reprehenderit anim consectetur excepteur duis veniam. Ipsum proident ullamco enim aute in amet pariatur officia consectetur. Aliqua nulla elit deserunt esse. Eiusmod culpa sunt cillum est velit ex magna veniam.'}</Text>
+                <Heading as='h4' fontSize='2xl' textAlign='left'>Contact</Heading>
+                    <List spacing={3}>
+                        <ListItem as={ChakraLink} boxSize='md' href='https://www.linkedin.com/in/dbriken/'>
+                                <ListIcon as={IconBrandLinkedin}/>www.linkedin.com/in/dbriken/
+                        </ListItem>
+                    </List>
+                    <List spacing={3}>
+                        <ListItem as={ChakraLink} boxSize='md' href='https://twitter.com/DBriken'>
+                                <ListIcon as={IconBrandTwitter}/>https://twitter.com/DBriken
+                        </ListItem>
+                    </List>
+                    <List spacing={3}>       
+                            <ListIcon as={IconMessage}/> dbriken.tech@gmail.com
+                    </List>
                 </Box>
             </VStack>
         </Container>
