b1260fe5251a585378097eafe6930e41d3836c97
David Briken <dbriken.tech@gmail.com>
2023-08-31 18:06:14 -0400

    changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
-import {Container, Box, Heading, Text, SimpleGrid, Image, Center, HStack, VStack, List, ListItem, ListIcon, Link as ChakraLink} from "@chakra-ui/react"
+import {Container, Heading, Text, SimpleGrid, Image, Center, HStack, VStack, List, ListItem, ListIcon, Link as ChakraLink} from "@chakra-ui/react"
 import React from 'react'
-import { IconBrandLinkedin, IconBrandTwitter, IconMessage} from "@tabler/icons-react";
+import { IconBrandLinkedin, IconBrandTwitter, IconInbox} from "@tabler/icons-react";
 import dbImage from '../images/headshot.jpg'
 import { Link } from "react-router-dom";
 export default function Home() {
@@ -8,39 +8,38 @@ export default function Home() {
         
         <Container width='-moz-max-content' centerContent>
            <VStack spacing='30px'>
-                <Box>
+                <Container  width='-moz-max-content'>
                     <HStack spacing='50px' alignItems='center'>
                         <Heading textAlign='center' fontSize='4xl'>David Briken</Heading>
                         <Image borderRadius='full' maxHeight='100px' maxWidth='100px' src={dbImage} alt="img"/>
                     </HStack>
-                </Box>
-                
-                
-                <Box>
+                </Container>
+                <Container>
                     <Heading as='h4' fontSize='2xl' textAlign='left'>About</Heading>
-                    <Text textAlign='justify' style={{textIndent: 20}}>{'\tDo commodo eiusmod amet amet proident qui consequat veniam occaecat excepteur enim dolor eu. Dolore eu aliquip occaecat ut sit fugiat. Reprehenderit anim consectetur excepteur duis veniam. Ipsum proident ullamco enim aute in amet pariatur officia consectetur. Aliqua nulla elit deserunt esse. Eiusmod culpa sunt cillum est velit ex magna veniam.'}</Text>
-                </Box>
-                <Box>
+                    <Text textAlign='justify' style={{textIndent: 20}}>{'\tDavid is an aspiring software developer passionate about creating products that help people! He has recently graduated with a bachelors degree in Information Systems from the University of Maryland, Baltimore County'}</Text>
+                </Container>
+                <Container>
                     <Heading as='h4' fontSize='2xl' textAlign='left'>Bio</Heading>
-                    <Text textAlign='justify' style={{textIndent: 20}}>{'\tDo commodo eiusmod amet amet proident qui consequat veniam occaecat excepteur enim dolor eu. Dolore eu aliquip occaecat ut sit fugiat. Reprehenderit anim consectetur excepteur duis veniam. Ipsum proident ullamco enim aute in amet pariatur officia consectetur. Aliqua nulla elit deserunt esse. Eiusmod culpa sunt cillum est velit ex magna veniam.'}</Text>
-                </Box>
-                <Box>
-                <Heading as='h4' fontSize='2xl' textAlign='left'>Contact</Heading>
+                    <Text textAlign='justify' style={{textIndent: 20}}>{''}</Text>
+                </Container>
+                <Container>
+                    <Heading as='h4' fontSize='2xl' textAlign='left'>Contact</Heading>
                     <List spacing={3}>
-                        <ListItem as={ChakraLink} boxSize='md' href='https://www.linkedin.com/in/dbriken/'>
-                                <ListIcon as={IconBrandLinkedin}/>www.linkedin.com/in/dbriken/
-                        </ListItem>
-                    </List>
-                    <List spacing={3}>
-                        <ListItem as={ChakraLink} boxSize='md' href='https://twitter.com/DBriken'>
-                                <ListIcon as={IconBrandTwitter}/>https://twitter.com/DBriken
-                        </ListItem>
-                    </List>
-                    <List spacing={3}>       
-                            <ListIcon as={IconMessage}/> dbriken.tech@gmail.com
-                            changes
-                    </List>
-                </Box>
+                            <ListItem as={ChakraLink} boxSize='md' href='https://www.linkedin.com/in/dbriken/'>
+                                    <ListIcon as={IconBrandLinkedin}/>@dbriken
+                            </ListItem>
+                        </List>
+                        <List spacing={3}>
+                            <ListItem as={ChakraLink} boxSize='md' href='https://twitter.com/DBriken'>
+                                    <ListIcon as={IconBrandTwitter}/>@dbriken
+                            </ListItem>
+                        </List>
+                        <List spacing={15}>  
+                            <ListItem as={ChakraLink} boxSize='md' href='mailto:dbriken.tech@gmail.com'>
+                                <ListIcon as={IconInbox}/>dbriken.tech@gmail.com
+                            </ListItem>
+                        </List>
+                </Container>
             </VStack>
         </Container>
         
