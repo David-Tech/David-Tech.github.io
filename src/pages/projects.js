66fc23607cd5c6af4c18504a780e7eb451de9fe9
David <david@Davids-Air-2.fios-router.home>
2023-11-08 13:54:58 -0500

    buttons added to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,30 +1,98 @@
-import {Container, Card, Heading, Image, CardBody, Text,Stack, useColorModeValue} from '@chakra-ui/react';
-import calculator from '../images/jsCalculator.png'
+import {
+  Container,
+  Card,
+  Heading,
+  Image,
+  CardBody,
+  Box,
+  Text,
+  Stack,
+  useColorModeValue,
+  Button,
+  Link,
+} from "@chakra-ui/react";
+import calculator from "../images/jsCalculator.png";
 
 export default function Projects() {
-  const bg = useColorModeValue('brand.50', 'whiteAlpha.50')
+  const bg = useColorModeValue("brand.50", "whiteAlpha.50");
 
   return (
-    <Container width='-moz-max-content' centerContent bg={bg} borderRadius='3xl'>
-      <Stack spacing='45px'>
-      <Heading >Projects</Heading>
-        <Card direction={{ base: 'column', sm: 'row' }} overflow='hidden' variant='outline'>
-        <Image src={calculator} 
-        borderRadius='20px' objectFit='cover' maxW={{base: '100%', sm: '200px'}}></Image>
-          <CardBody>
-            <Heading size='md'>Calculator WebApp</Heading>
-            <Text>Summary of project and technologies used</Text>
-          </CardBody>
+    <Container
+      width="-moz-max-content"
+      centerContent
+      bg={bg}
+      borderRadius="3xl"
+    >
+      <Stack spacing="45px">
+        <Heading>Projects</Heading>
+        <Card
+          direction={{ base: "column", sm: "row" }}
+          overflow="hidden"
+          variant="outline"
+        >
+          <Image
+            src={calculator}
+            borderRadius="20px"
+            objectFit="cover"
+            marginRight='10px'
+            maxW={{ base: "100%", sm: "200px" }}
+          ></Image>
+          <Stack alignItems="center">
+            <Heading size="md">Calculator WebApp</Heading>
+            <Text>
+              Basic calculator for the browser capable of multiplication,
+              division, addition & subtraction. Created using HTML/CSS &
+              Javascript
+            </Text>
+            <Box alignItems="center">
+              <Button>
+                <Link href="https://david-tech.github.io/calculator/">
+                  Visit
+                </Link>
+              </Button>
+              <Button>
+                <Link href="https://github.com/David-Tech/calculator">
+                  Source
+                </Link>
+              </Button>
+            </Box>
+          </Stack>
         </Card>
-        <Card direction={{ base: 'column', sm: 'row' }} overflow='hidden' variant='outline'>
-        <Image src={calculator} 
-        borderRadius='20px' objectFit='cover' maxW={{base: '100%', sm: '200px'}}></Image>
-          <CardBody>
-            <Heading size='md'>Calculator WebApp</Heading>
-            <Text>Summary of project and technologies used</Text>
-          </CardBody>
+        <Card
+          direction={{ base: "column", sm: "row" }}
+          overflow="hidden"
+          variant="outline"
+          
+        >
+          <Image
+            src={calculator}
+            borderRadius="20px"
+            objectFit="cover"
+            marginRight='10px'
+            maxW={{ base: "100%", sm: "200px" }}
+          ></Image>
+          <Stack alignItems="center">
+            <Heading size="md">Calculator WebApp</Heading>
+            <Text>
+              Basic calculator for the browser capable of multiplication,
+              division, addition & subtraction. Created using HTML/CSS &
+              Javascript
+            </Text>
+            <Box alignItems="center">
+              <Button marginRight='.5rem'>
+                <Link href="https://david-tech.github.io/calculator/">
+                  Visit
+                </Link>
+              </Button>
+              <Button>
+                <Link href="https://github.com/David-Tech/calculator">
+                  Source
+                </Link>
+              </Button>
+            </Box>
+          </Stack>
         </Card>
       </Stack>
     </Container>
-  )
+  );
 }
